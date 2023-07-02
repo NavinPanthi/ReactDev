ab4bd8da8764fbcd4f11228f910032747644803a
Navin Panthi <panthinabin341@gmail.com>
2023-07-02 22:25:15 +0545

    onSquareClick function to the Square component’s props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
+//  Whole page  component.
 function App() {
   return (
     <div className="App">
@@ -10,30 +11,39 @@ function App() {
     </div>
   );
 }
+//Component of board containing 9 squares component.
 function Board() {
+  // Creating 9 null elements in arrays for 9 squares.
+  // So initially all squares such as squares[0], square[1] will have null values.
+
   const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <div className="d-flex flex-column">
       <div className="d-flex flex-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
-        <Square value={squares[3]} />
       </div>
       <div className="d-flex flex-row">
+        <Square value={squares[3]} />
         <Square value={squares[4]} />
         <Square value={squares[5]} />
-        <Square value={squares[6]} />
       </div>
       <div className="d-flex flex-row">
+        <Square value={squares[6]} />
         <Square value={squares[7]} />
         <Square value={squares[8]} />
-        <Square value={squares[9]} />
       </div>
     </div>
   );
 }
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+// Square component for board.
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
-
 export default App;
