241c77fc17f555898dd434aa60fbcf2014822791
Navin Panthi <panthinabin341@gmail.com>
2023-07-03 21:58:29 +0545

    overwritten of squares box is solved.

--- src/App.js
+++ src/App.js
@@ -15,25 +15,45 @@ function App() {
 function Board() {
   // Creating 9 null elements in arrays for 9 squares.
   // So initially all squares such as squares[0], square[1] will have null values.
-
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  function handleClick(i) {
+    // To implement the concept of immutability, we have copy the array of squares to nextSquares by using slice method.
+
+    const nextSquares = squares.slice();
+    console.log(nextSquares);
+    if (nextSquares[i] == "X" || nextSquares[i] == "O") {
+      return;
+    }
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setXIsNext(!xIsNext);
+    setSquares(nextSquares);
+    console.log(nextSquares);
+  }
 
   return (
     <div className="d-flex flex-column">
       <div className="d-flex flex-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        {/* The following will cause error as function is called not passed so it will be rendered in loop. */}
+        {/* <Square value={squares[0]} onSquareClick={handleClick(0)} /> */}
+        {/* The following will work because we have passes a prop by defining the function. */}
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="d-flex flex-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="d-flex flex-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </div>
   );
