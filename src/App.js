28d554edfb5ff997aa41e418f7abb5a2b55f17ff
Navin Panthi <panthinabin341@gmail.com>
2023-07-07 12:37:15 +0545

    final wrap up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import { useState } from "react";
 import "./App.css";
 
-//  Whole page  component.
-function App() {
+// Square component for board.
+function Square({ value, index, onSquareClick }) {
+  let className = "square btn-lg";
+  if (index === 0) {
+    className = "firstSquare square btn-lg";
+  } else if (index === 2) {
+    className = "thirdSquare square btn-lg";
+  } else if (index === 6) {
+    className = "seventhSquare square btn-lg";
+  } else if (index === 8) {
+    className = "ninthSquare square btn-lg";
+  }
   return (
-    <div className="App">
-      <header className="App-header">Tic Tac Tutorial Game</header>
-      <hr />
-      <Game />
-    </div>
+    <button className={className} onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
-
 //Component of board containing 9 squares component.
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     // To implement the concept of immutability, we have copy the array of squares to nextSquares by using slice method.
+
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -45,15 +53,22 @@ function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div>{statuss}</div>
-
       <div className="d-flex flex-column">
         <div className="d-flex flex-row">
           {/* The following will cause error as function is called not passed so it will be rendered in loop. */}
           {/* <Square value={squares[0]} onSquareClick={handleClick(0)} /> */}
           {/* The following will work because we have passes a prop by defining the function. */}
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square
+            value={squares[0]}
+            index={0}
+            onSquareClick={() => handleClick(0)}
+          />
           <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          <Square
+            value={squares[2]}
+            index={2}
+            onSquareClick={() => handleClick(2)}
+          />
         </div>
         <div className="d-flex flex-row">
           <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
@@ -61,76 +76,63 @@ function Board({ xIsNext, squares, onPlay }) {
           <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
         </div>
         <div className="d-flex flex-row">
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square
+            value={squares[6]}
+            index={6}
+            onSquareClick={() => handleClick(6)}
+          />
           <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          <Square
+            value={squares[8]}
+            index={8}
+            onSquareClick={() => handleClick(8)}
+          />
         </div>
       </div>
     </>
   );
 }
-// Square component for board.
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square btn-lg" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
+
 function Game() {
   // Creating 9 null elements in arrays for 9 squares.
   // So initially all squares such as squares[0], square[1] will have null values.
-  const [xIsNext, setXIsNext] = useState(true);
+
   // All the steps of clicked events in buttons are stored.
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   // THe current squares variable store the last state of history i.e. present steps of clicked button.
   const currentSquares = history[currentMove];
-
-
-  console.log("History");
-  console.log(history);
-  console.log("Currentsquares");
-  console.log(currentSquares);
-  console.log("Length");
-  console.log(history.length);
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    // Every array when button clicked is added to history.
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
   // Here history array is mapped to a function where  each time each element (i.e array) of history array is passed
   //  and move is the index of element passed to history array.
   const moves = history.map((squares, move) => {
     let description;
+
     if (move > 0) {
       description = "Go to move" + move;
-      return (
-        <li key={move}>
-          <button className="goto btn btn-light" onClick={() => jumpTo(move)}>
-            {description}
-          </button>
-        </li>
-      );
     } else {
       description = "Start the game.";
-      return (
-        <li key={move}>
-          <button className="start btn btn-light " onClick={() => jumpTo(move)}>
-            {description}
-          </button>
-        </li>
-      );
     }
+    if (move === currentMove && move !== 0) {
+      description = "You are at move" + move;
+    }
+    return (
+      <li key={move}>
+        <button className="start btn" onClick={() => jumpTo(move)}>
+          {description}
+        </button>
+      </li>
+    );
   });
 
   return (
@@ -144,16 +146,17 @@ function Game() {
     </div>
   );
 }
+
 function calculateWinner(squares) {
   const matches = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
   for (let i = 0; i < matches.length; i++) {
     const [a, b, c] = matches[i];
@@ -163,4 +166,14 @@ function calculateWinner(squares) {
   }
   return null;
 }
+//  Whole page  component.
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header mb-5">Tic Tac Tutorial Game</header>
+      <Game />
+    </div>
+  );
+}
+
 export default App;
