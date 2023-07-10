185a6072df9e358fb4e3a3667c1a343e5d1bb536
Navin Panthi <panthinabin341@gmail.com>
2023-07-10 15:54:01 +0545

    giving a component multiple state variables

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import "./App.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <Toolbar />
-    </div>
-  );
-}
+import { useState } from "react";
+import { sculptureList } from "./data";
 
 // PASSING EVENT HANDLERS AS PROPS .
 // function Button({ onClick, children }) {
@@ -31,27 +25,60 @@ export default function App() {
 // }
 
 // STOPPING PROPAGATION i.e. auto event handler pass to parent element.
+// function Button({ onClick, children }) {
+//   return (
+//     <button
+//       onClick={(e) => {
+//         e.stopPropogation();
+//         onClick();
+//       }}
+//     >
+//       {children}
+//     </button>
+//   );
+// }
+
+// function Toolbar() {
+//   return (
+//     <div
+//       className="container bg-primary"
+//       onClick={() => alert("Toolbar is clicked.")}
+//     >
+//       <Button onClick={() => alert("Playing movie.")}>Play movie.</Button>
+//       <Button onClick={() => alert("Playing music.")}> Play Song.</Button>
+//     </div>
+//   );
+// }
+// GIVING A COMPONENT MULTIPLE STATE VARIABLES
+
 function Button({ onClick, children }) {
-  return (
-    <button
-      onClick={(e) => {
-        e.stopPropogation();
-        onClick();
-      }}
-    >
-      {children}
-    </button>
-  );
+  return <button onClick={onClick}>{children}</button>;
 }
 
-function Toolbar() {
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [show, setShow] = useState(false);
+
+  let sculpture = sculptureList[index];
+
+  function handleNext() {
+    setIndex(index + 1);
+  }
+  function showMore() {
+    setShow(!show);
+  }
   return (
-    <div
-      className="container bg-primary"
-      onClick={() => alert("Toolbar is clicked.")}
-    >
-      <Button onClick={() => alert("Playing movie.")}>Play movie.</Button>
-      <Button onClick={() => alert("Playing music.")}> Play Song.</Button>
+    <div>
+      <Button onClick={handleNext}>Next</Button>
+      <h2>
+        {sculpture.name} by {sculpture.artist}
+      </h2>
+      <h3>
+        {index + 1} of {sculptureList.length}
+      </h3>
+      <Button onClick={showMore}>{show ? "Hide" : "Show"} Details</Button>
+      {show && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
     </div>
   );
 }
