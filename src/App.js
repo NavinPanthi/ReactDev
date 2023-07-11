7ea62f0cbb1e4bf2d092da0b515a6d5b29e637ff
Navin Panthi <panthinabin341@gmail.com>
2023-07-11 12:54:50 +0545

    using useState in form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useState } from "react";
-import { sculptureList } from "./data";
+// import { sculptureList } from "./data";
 
 // PASSING EVENT HANDLERS AS PROPS .
 // function Button({ onClick, children }) {
@@ -51,56 +51,90 @@ import { sculptureList } from "./data";
 // }
 // GIVING A COMPONENT MULTIPLE STATE VARIABLES
 
-function Button({ onClick, children }) {
-  return <button onClick={onClick}>{children}</button>;
-}
+// function Button({ onClick, children }) {
+//   return <button onClick={onClick}>{children}</button>;
+// }
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [show, setShow] = useState(false);
-  const [isDisabled, setIsDisabled] = useState(false);
+// export default function Gallery() {
+//   const [index, setIndex] = useState(0);
+//   const [show, setShow] = useState(false);
+//   const [isDisabled, setIsDisabled] = useState(false);
 
-  let sculpture = sculptureList[index];
+//   let sculpture = sculptureList[index];
 
-  function handleNext() {
-    if (index < sculptureList.length - 1) {
-      setIndex(index + 1);
-      console.log(index);
-    } else {
-      setIsDisabled(!isDisabled);
-    }
+//   function handleNext() {
+//     if (index < sculptureList.length - 1) {
+//       setIndex(index + 1);
+//       console.log(index);
+//     } else {
+//       setIsDisabled(!isDisabled);
+//     }
+//   }
+//   function handlePrevious() {
+//     if (index>0 ) {
+//       setIndex(index - 1);
+//       console.log(index);
+//     } else {
+//       setIsDisabled(!isDisabled);
+//     }
+//   }
+//   function showMore() {
+//     setShow(!show);
+//   }
+
+//   return (
+//     <div>
+//       <div>
+//         <Button onClick={handleNext} disabled={isDisabled}>
+//           Next
+//         </Button>
+//         <Button onClick={handlePrevious} disabled={isDisabled}>
+//           Previous
+//         </Button>
+//       </div>
+//       <h2>
+//         {sculpture.name} by {sculpture.artist}
+//       </h2>
+//       <h3>
+//         {index + 1} of {sculptureList.length}
+//       </h3>
+//       <Button onClick={showMore}>{show ? "Hide" : "Show"} Details</Button>
+//       <img src={sculpture.url} alt={sculpture.alt} />
+//       {show && <p>{sculpture.description}</p>}
+//     </div>
+//   );
+// }
+
+//using useState in form
+export default function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
   }
-  function handlePrevious() {
-    if (index>0 ) {
-      setIndex(index - 1);
-      console.log(index);
-    } else {
-      setIsDisabled(!isDisabled);
-    }
+  function handleSecondNameChange(e) {
+    setSecondName(e.target.value);
   }
-  function showMore() {
-    setShow(!show);
+  function handleReset(){
+    setFirstName("");
+    setSecondName("");
   }
-
   return (
-    <div>
-      <div>
-        <Button onClick={handleNext} disabled={isDisabled}>
-          Next
-        </Button>
-        <Button onClick={handlePrevious} disabled={isDisabled}>
-          Previous
-        </Button>
-      </div>
-      <h2>
-        {sculpture.name} by {sculpture.artist}
-      </h2>
-      <h3>
-        {index + 1} of {sculptureList.length}
-      </h3>
-      <Button onClick={showMore}>{show ? "Hide" : "Show"} Details</Button>
-      <img src={sculpture.url} alt={sculpture.alt} />
-      {show && <p>{sculpture.description}</p>}
-    </div>
+    <>
+      <input
+        value={firstName}
+        placeholder="FirstName"
+        onChange={handleFirstNameChange}
+      />
+      <input
+        value={secondName}
+        placeholder="FirstName"
+        onChange={handleSecondNameChange}
+      />
+      <p>Hi {firstName + secondName} </p>
+      <button onClick={handleReset}>
+        Reset
+      </button>
+    </>
   );
 }
