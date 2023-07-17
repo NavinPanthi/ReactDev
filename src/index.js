61a8cedf28da84099957693dff1e9f58927cb197
Navin Panthi <panthinabin341@gmail.com>
2023-07-17 13:52:06 +0545

    ToDoList App using Immer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import ToDo from "./ToDo";
+import ToDo from "./ToDoApp/UsingImmer/ToDo";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
