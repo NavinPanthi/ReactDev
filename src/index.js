d026f6041f8253797acbfc5e35a0177e0814bc0f
Navin Panthi <panthinabin341@gmail.com>
2023-07-18 15:47:25 +0545

    adding and removing css class

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Form from "./HandlingForm";
+import AddRemoveCssClass from "./AddRemoveCssClass";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Form />
+    <AddRemoveCssClass />
   </React.StrictMode>
 );
 
