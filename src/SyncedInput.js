38a7a4dec9d0f4d62320fb670a1bd359fd1f7000
Navin Panthi <panthinabin341@gmail.com>
2023-07-21 22:05:15 +0545

    FilterList file

--- src/SyncedInput.js
+++ src/SyncedInput.js
@@ -1,25 +1,24 @@
 import { useState } from "react";
 
-export default function SyncedInput(){
-    const [text, setText] = useState('');
-    function handleChange(e){
-        setText(e.target.value);
-    }
-    return(
-        <>
-        <Input text label="First input" onChange={handleChange}/>
-        <Input text label = "Second input" onChange={handleChange}/>
-        </>
-    );
+export default function SyncedInput() {
+  const [text, setText] = useState("");
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+  return (
+    <>
+      <Input value={text} label="First input" onChange={handleChange} />
+      <Input value={text} label="Second input" onChange={handleChange} />
+    </>
+  );
 }
 
-function Input({text, label, onChange}){
-  
-    return(
-        <>
-            {label}
-            <input value={text} onChange={onChange}/>
-            <input value={text} onChange={onChange}/>
-        </>
-    );
+function Input({ value, label, onChange }) {
+  return (
+    <label>
+      {label}
+      {" "}
+      <input value={value} onChange={onChange} />
+    </label>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import LetterSelection from "./LetterSelection.js";
+import FilterList from "./FilterList.js";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <LetterSelection />
+    <FilterList />
   </React.StrictMode>
 );
 
