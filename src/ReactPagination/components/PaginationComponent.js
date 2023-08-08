261a705db52eaf32cad4fe47df6b8189476985b3
Navin Panthi <panthinabin341@gmail.com>
2023-08-08 13:09:17 +0545

    load more button to load more todos

--- src/ReactPagination/components/PaginationComponent.js
+++ src/ReactPagination/components/PaginationComponent.js
@@ -54,36 +54,81 @@ const PaginationComponent = () => {
       setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
     }
   };
-
+  let pageIncrementButton = null;
+  if (pages.length > maxPageNumberLimit) {
+    pageIncrementButton = (
+      <li onClick={handleNext} className="p-2 mx-1">
+        &hellip;
+      </li>
+    );
+  }
+  let pageDecrementButton = null;
+  if (minPageNumberLimit !== 0) {
+    pageDecrementButton = (
+      <li onClick={handlePrev} className="p-2 mx-1 ">
+        &hellip;
+      </li>
+    );
+  }
+  const handleLoadMore = () => {
+    setItemsPerPage(itemsPerPage + 10);
+  };
   const renderPageNumbers = () => {
     return (
-      <ul className="flex flex-row shadow-md mt-9 mx-auto w-50 rounded-xl divide-x  divide-gray-300 overflow-hidden text-sm">
-        <button className="p-2 active:bg-gray-300" onClick={handlePrev}>
-          Prev
-        </button>
+      <>
+        <ul className="flex shadow-lg text-indigo-900 flex-row mt-9 mx-auto w-50 rounded-xl divide-x  divide-gray-300 overflow-hidden text-sm">
+          <button
+            className={`p-2  ${
+              currentPage === pages[0] ? "opacity-50" : "active:bg-gray-300"
+            }`}
+            onClick={handlePrev}
+            disabled={currentPage === pages[0]}
+          >
+            Prev
+          </button>
+          {pageDecrementButton}
+          {pages.map((number) => {
+            if (
+              number < maxPageNumberLimit + 1 &&
+              number > minPageNumberLimit
+            ) {
+              return (
+                <li
+                  key={number}
+                  id={number}
+                  className={
+                    currentPage === number ? listClassNameActive : listClassName
+                  }
+                  onClick={handleClick}
+                >
+                  {number}
+                </li>
+              );
+            } else {
+              return null;
+            }
+          })}
 
-        {pages.map((number) => {
-          if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
-            return (
-              <li
-                key={number}
-                id={number}
-                className={
-                  currentPage === number ? listClassNameActive : listClassName
-                }
-                onClick={handleClick}
-              >
-                {number}
-              </li>
-            );
-          } else {
-            return null;
-          }
-        })}
-        <button className="p-2 active:bg-gray-300" onClick={handleNext}>
-          Next
+          {pageIncrementButton}
+          <button
+            className={`p-2  ${
+              currentPage === pages[pages.length - 1]
+                ? "opacity-30"
+                : "active:bg-gray-300"
+            }`}
+            onClick={handleNext}
+            disabled={currentPage === pages[pages.length - 1]}
+          >
+            Next
+          </button>
+        </ul>
+        <button
+          onClick={handleLoadMore}
+          className="shadow-lg p-2 mt-5 rounded-xl hover:bg-gray-100 text-indigo-900 mx-auto text-sm  w-40 flex justify-center"
+        >
+          Load more
         </button>
-      </ul>
+      </>
     );
   };
 
