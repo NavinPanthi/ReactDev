02576706bb445c060761ae282b2d79613e7244d4
Navin Panthi <panthinabin341@gmail.com>
2023-08-08 11:06:22 +0545

    prev and next button added

--- src/ReactPagination/components/PaginationComponent.js
+++ src/ReactPagination/components/PaginationComponent.js
@@ -15,50 +15,77 @@ const renderData = (data) => {
 };
 
 const PaginationComponent = () => {
-  const [data, setData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(10);
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
       .then((json) => setData(json));
   }, []);
-
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [pageNumberLimit, setPageNumberLimit] = useState(5);
+  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
+  const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
   const pages = [];
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexofFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexofFirstItem, indexOfLastItem);
+
+  let listClassName = "p-2 text-center w-full cursor-pointer ";
+  let listClassNameActive = listClassName + " bg-gray-300";
   for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
     pages.push(i);
   }
-  let listClassName = "p-2 text-center w-full cursor-pointer ";
-  let listClassNameActive = "p-2 text-center w-full cursor-pointer bg-gray-300";
-  const renderPageNumbers = () => {
-    return (
-      <ul class="flex flex-row shadow-md mt-2 rounded-xl divide-x  divide-gray-300 overflow-hidden text-sm">
-        <button className="p-2 active:bg-gray-300">Prev</button>
-        {pages.map((number) => {
-          return (
-            <li
-              key={number}
-              id={number}
-              className={
-                currentPage === number ? listClassNameActive : listClassName
-              }
-              onClick={handleClick}
-            >
-              {number}
-            </li>
-          );
-        })}
-        <button className="p-2 active:bg-gray-300">Next</button>
-      </ul>
-    );
-  };
 
   const handleClick = (e) => {
     setCurrentPage(Number(e.target.id));
   };
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexofFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = data.slice(indexofFirstItem, indexOfLastItem);
+  const handlePrev = () => {
+    setCurrentPage(currentPage - 1);
+    if ((currentPage - 1) % pageNumberLimit === 0) {
+      setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+      setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+    }
+  };
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
+    if (currentPage + 1 > maxPageNumberLimit) {
+      setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+      setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+    }
+  };
+
+  const renderPageNumbers = () => {
+    return (
+      <ul className="flex flex-row shadow-md mt-9 mx-auto w-50 rounded-xl divide-x  divide-gray-300 overflow-hidden text-sm">
+        <button className="p-2 active:bg-gray-300" onClick={handlePrev}>
+          Prev
+        </button>
+
+        {pages.map((number) => {
+          if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
+            return (
+              <li
+                key={number}
+                id={number}
+                className={
+                  currentPage === number ? listClassNameActive : listClassName
+                }
+                onClick={handleClick}
+              >
+                {number}
+              </li>
+            );
+          } else {
+            return null;
+          }
+        })}
+        <button className="p-2 active:bg-gray-300" onClick={handleNext}>
+          Next
+        </button>
+      </ul>
+    );
+  };
 
   return (
     <div className="container">
